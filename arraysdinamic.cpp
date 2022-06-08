66b3ca2820f487953334b70827a28f3c0b469974
mklg03 <98665287+mklg03@users.noreply.github.com>
2022-06-07 22:10:31 -0400

    Add files via upload

---
+++ arraysdinamic.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int n, i , cont = 0;
+
+    float *puntero;
+
+    printf("Indique el numero de edades\n");
+    scanf("%d", &n);
+
+    puntero = (float *) malloc (n* sizeof(float));
+
+
+    for( i= 0 ; i < n; i++){
+
+        printf("Edad #: %d\n", i +1 );
+        scanf("%f", &puntero[i]);
+    
+    }
+
+
+    for( i= 0 ; i < n; i++ ){
+
+        if( puntero[i] >= 18){
+            cont++;
+        }
+    }
+
+
+    printf("Hay  %d personas mayores de edad", cont);
+
+    free(puntero);
+
+return 0 ;
+}
